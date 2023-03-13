c1a3cfd82fb632c7abf184145c29ae10ca240c41
Admin <Admin@DESKTOP-A58RGQ7>
2023-03-13 21:14:40 +0530

    First commit

---
+++ src/pyramid.java
@@ -0,0 +1,16 @@
+
+public class pyramid {
+	public static void main(String[] args) {
+		int row=1;
+		int col=4;
+		for(int i=1; i<=row; i++) {
+			for(col=1; col<=3; col++) {
+				System.out.print(row);
+				
+			}
+			
+		}
+		
+	}
+}
+		
